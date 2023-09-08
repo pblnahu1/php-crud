749e568b4c723d334841ce4030ef0fffecda1d88
Pablo Torrez <122138661+pblnahu1@users.noreply.github.com>
2023-09-08 09:46:38 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -17,7 +17,7 @@ if (isset($_POST['enviar-registro'])) {
         $emailUsuario = trim($_POST['email']);
         $claveUsuario = trim($_POST['clave']);
         $provinciaUsuario = trim($_POST['provincia']);
-        $fechaIngreso = date("d/m/y");
+        $fechaIngreso = date("Y/m/d");
 
         $consulta = "INSERT INTO datos_usuarios(nombre_usuario, apellido_usuario, edad_usuario, email_usuario, clave_usuario, provincia_usuario, fecha_ingreso) VALUES('$nombreUsuario', '$apellidoUsuario', '$edadUsuario', '$emailUsuario', '$claveUsuario', '$provinciaUsuario', '$fechaIngreso')";
 

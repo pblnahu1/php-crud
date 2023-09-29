be58c006c84aac1203b12eefb8b7219d665bd27d
Pablo Torrez <122138661+pblnahu1@users.noreply.github.com>
2023-09-29 10:18:24 -0300

    Update cerrar_sesion.php

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -3,6 +3,7 @@
     session_unset();
     // session_destroy();
     $_SESSION['mensaje'] = 'Se ha cerrado la sesión';
+    //unset($_SESSION['email_usuario']);
     header("Location: login.php");
     exit();
 ?>

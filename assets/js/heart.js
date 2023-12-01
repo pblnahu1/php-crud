baca1c8e1ca70e798e1d5912122a60305d658955
Pablo Torrez <pablotorrez90@outlook.com>
2023-12-01 07:56:35 -0300

    config

--- assets/js/heart.js
+++
@@ -1,7 +0,0 @@
-const heart = document.getElementsByClassName('heart');
-
-for (let i = 0; i < heart.length; i++){
-    heart[i].addEventListener('click', function () {
-        this.classList.toggle('selected');
-    })
-}
